c45f323c31387f893150146c9b9347c13f1b4636
frotle <jeffrey.glassford@gmail.com>
2026-05-23 13:57:50 -0400

    Partially running Turret code - do not use.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/TurretSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/TurretSubsystem.java
@@ -34,7 +34,7 @@ public class TurretSubsystem extends SubsystemBase {
         turret.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Sets the motor to PIDF values
-        turret.setVelocityPIDFCoefficients(1.0, 0.1, 0.00, 1.0);//needs to be tuned for the turret
+        turret.setVelocityPIDFCoefficients(8.0, 0.1, 0.00, 1.0);//needs to be tuned for the turret
 
         turret.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
